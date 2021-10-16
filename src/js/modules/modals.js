abef97f76c1934f0402aa885ad9d8211599fa8f3
unknown <olena.denysova88@gmail.com>
2021-10-16 13:11:41 +0200

    modals

---
+++ src/js/modules/modals.js
@@ -0,0 +1,100 @@
+const modals = () => {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+        const trigger = document.querySelectorAll(triggerSelector);
+        const modal = document.querySelector(modalSelector);
+        const close = document.querySelector(closeSelector);
+        const windows = document.querySelectorAll('[data-modal]');
+        const scroll = calcScroll();
+
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if(e.target) {
+                    e.preventDefault();
+                }
+                //close all opened popups
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+    
+                modal.style.display = "block";
+                document.body.style.overflow = "hidden";
+                document.body.style.marginRight = `${scroll}px`;
+            });
+        });
+        
+        close.addEventListener('click', () => {
+            //close all opened popups
+            windows.forEach(item => {
+                item.style.display = 'none';
+            });
+
+            modal.style.display = "none";
+            document.body.style.overflow = "";
+            document.body.style.marginRight = '0px';
+        });
+
+        //close modal on click on screen
+        modal.addEventListener('click', (e) => {
+            if(e.target === modal && closeClickOverlay ) {
+                //close all opened popups
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+
+                modal.style.display = "none";
+                document.body.style.overflow = "";
+               /* document.body.classList.remove('modal-open'); */
+               document.body.style.marginRight = '0px';
+            }
+        })
+
+        // close modal on Esc key click
+        document.addEventListener('keydown', e =>
+        e.key === "Escape" ? modal.style.display = "none" : false
+        );
+
+    }
+
+    //show modal on screen after some time
+    function showModalByTime(selector, time) {
+        setTimeout(() => {
+            //do not show if any modal is already opened
+            let display;
+            document.querySelectorAll('[data-modal').forEach(item => {
+                if(getComputedStyle(item).display !== 'none') {
+                    display = 'block';
+                }
+            });
+
+            if(!display) {
+                document.querySelector(selector).style.display = 'block';
+                document.body.style.overflow = "hidden";
+            }
+           
+        }, time);
+    }
+
+    //to fix bug with jumping window after modal shown
+    function calcScroll() {
+        let div = document.createElement('div');
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        //whole width - content width(not includes scroll)
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+
+        return scrollWidth;
+    }
+
+    bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
+    bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
+    
+    showModalByTime('.popup-consultation', 5000); 
+
+};
+
+export default modals;

425c80dfae320865613c171887eb5dca210fdfb9
unknown <olena.denysova88@gmail.com>
2021-11-03 21:38:54 +0100

    calc

---
+++ src/js/modules/calc.js
@@ -0,0 +1,11 @@
+const calc = (size, material, options, promocode, result) => {
+    const sizeBlock = document.querySelector(size);
+    const materialBlock = document.querySelector(material);
+    const optionsBlock = document.querySelector(options);
+    const promocodeBlock = document.querySelector(promocode);
+    const resultBlock = document.querySelector(result);
+
+    let sum = 0;
+};
+
+export default calc;
